009cbcc6fd03f362ff739cf0cffc6a5f3a3d50af
wporta06 <portawalid@gmail.com>
2022-03-31 16:52:45 +0000

    first commit

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+//console.log(io)
+
+// events (connection)
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+  // console.log(`Socket=> ${socket} `);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User "${data.username}" with ID: ${socket.id} joined room: ${data.room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    // console.log(data)
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+
+});
+
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
